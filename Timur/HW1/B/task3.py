9685be17daf0bbf29218f17107ef8b8c3f9b751c
tkhal3_nike <timur.khalilov@nike.com>
2023-07-24 13:51:05 +0300

    hw1

--- Timur/HW1/B/task3.py
+++ Timur/HW1/B/task3.py
@@ -1,5 +1,6 @@
 val1, val2, val3 = map(lambda x: int(x), input().split(' '))
 
+
 def validation_data(val1, val2, val3):
     if (
             (0 < val1 <= 31)
@@ -14,11 +15,12 @@ def validation_data(val1, val2, val3):
 def is_clear_date(val1, val2, val3):
     if validation_data(val1, val2, val3):
         if (
-            ((val1 > 12) and (val2 <= 12))
-            or (val2 > 12) and (val1 <= 12)
-            or (val1 == val2)
+                ((val1 > 12) and (val2 <= 12))
+                or (val2 > 12) and (val1 <= 12)
+                or (val1 == val2)
         ):
             return "1"
         return "0"
+
 
 print(is_clear_date(val1, val2, val3))
